0d99b6bdbe6e49d2ba62f20ed185a67a043fb165
Chris Jagusch <cjjagusc@mtu.edu>
2015-07-14 17:15:22 -0400

    adding js data files and custom file for custom jquery stuff

---
+++ js/class_data.js
@@ -0,0 +1,38 @@
+var class_data = {
+	"barbarian": {
+
+	},
+	"bard": {
+
+	},
+	"cleric": {
+
+	},
+	"druid": {
+
+	},
+	"fighter": {
+
+	},
+	"monk": {
+
+	},
+	"paladin": {
+
+	},
+	"ranger": {
+
+	},
+	"rogue": {
+
+	},
+	"sorcerer": {
+
+	},
+	"warlock": {
+
+	},
+	"wizard": {
+
+	},
+}
---
+++ js/custom.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+	//Main Bootstrap/jQuery custom code
+});
