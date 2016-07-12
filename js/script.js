0aef5e014c583b6eec5976e0098f4fbb63602c70
Christopher Jagusch <cjjagusc@mtu.edu>
2016-07-12 18:58:31 -0400

    added favicon, beginning of rules stuff, and changelog dropdown. Neato burrito!

--- js/script.js
+++ js/script.js
@@ -4,23 +4,30 @@ var url = 'http://dl.dropboxusercontent.com/s/skhhsn2qi3ngcmx/data.json?dl=1';
 jQuery(document).ready(function($){
   if(sessionStorage.getItem("compendium") == null || sessionStorage.getItem("compendium") == "undefined"){
     $.when(
-      $.ajax({
-        type: 'GET',
-        cache: true,
-        // url: 'https://jsonp.afeld.me/?url=http://dl.dropboxusercontent.com/s/skhhsn2qi3ngcmx/data.json&raw=1',
-        url: 'data.json',
-        dataType: 'json',
-      })
-    ).then(function(data){
+      // $.ajax({
+      //   type: 'GET',
+      //   cache: true,
+      //   // url: 'https://jsonp.afeld.me/?url=http://dl.dropboxusercontent.com/s/skhhsn2qi3ngcmx/data.json&raw=1',
+      //   url: 'data.json',
+      //   dataType: 'json',
+      // })
+      $.get('data.json'),
+      $.get('rules.json')
+    ).then(function(data, rules){
       //cache JSON for faster load times
+      var c;
+      c = $.extend(c, data[0], rules[0]);
+
       try{
-        sessionStorage.setItem("compendium", JSON.stringify(data));
+        sessionStorage.setItem("compendium", JSON.stringify(c));
       } catch(e) {
         console.log("Couldn't cache the JSON file");
       }
       //assign JSON object
-      init(data);
-    })
+      init(c);
+    }).fail(function(){
+      console.log("failure loading data")
+    });
   }else{ //pull from local cache
     init(JSON.parse(sessionStorage.getItem("compendium")));
   }
@@ -33,6 +40,8 @@ jQuery(document).ready(function($){
     var sp = getInfo(suggestion);
     createCard(sp);
   });
+
+  populateChangelog();
 });
 
 function init(data){
@@ -59,54 +68,74 @@ function createCard(info){
   $(".typeahead").blur();
   var firstText = true;
 
+  var type = info.type;
+  delete info.type;
+
+  if(type=="rules"){
+    $.each(info, function(k,v){
+      if(k == "related") { //create links to related content
+        var str = "";
+        var len = v.length;
+        $.each(v, function(key,val){
+          str += "<a href='javascript:search(\""+val+"\")'>"+val+"</a>" + ((key == len - 1) ? "": ", ");
+        });
+        $("#results").append("<div class='card-"+k+"'><span id='card-"+k+"-key'><b>" + k.toTitleCase() + "</b>:&nbsp;</span><span id='card-"+k+"-value'>" + str + "</span></div>");
+      }
+      else
+      {
+        $("#results").append("<div class='card-"+k+"'><span id='card-"+k+"-key'><b>" + k.toTitleCase() + "</b>:&nbsp;</span><span id='card-"+k+"-value'>" + v + "</span></div>");
+      }
+    })
+  }else{
   // use reflection to build out info card
   for(var data in info){
-    if(typeof info[data] == "string"){ // simple k, v pair
-      $("#results").append("<div class='card-"+data+"'><span id='card-"+data+"-key'><b>" + data.toTitleCase() + "</b>:&nbsp;</span><span id='card-"+data+"-value'>" + info[data] + "</span></div>");
-      $("#card-link");
-    }else if(data == "text"){
-      for(var str in info[data]){
-        $("#results").append("<div>"+info[data][str]+"</div>")
-      }
-    }else if(info[data] != undefined){ // has object or array of objects inside val
-      var str = "<table><tbody><tr><th class='trait'>"+data.toTitleCase()+":</th><td><table><tbody><tr>";
-      for(var k in info[data]){
-        var val = info[data][k];
-        if(val == "") continue;
-        if(val.name){
-          str += "<tr><td><b>" + val.name + "</b></td></tr>";
+      if(typeof info[data] == "string"){ // simple k, v pair
+        $("#results").append("<div class='card-"+data+"'><span id='card-"+data+"-key'><b>" + data.toTitleCase() + "</b>:&nbsp;</span><span id='card-"+data+"-value'>" + info[data] + "</span></div>");
+      }else if(data == "text"){
+        for(var str in info[data]){
+          $("#results").append("<div>"+info[data][str]+"</div>")
         }
-        if(val.text){
-          str += "<tr><td>"+val.text+"</td></tr>";
-        }
-        if(val.attack){
-          str += "<tr><td><i>"+val.attack+"</i></td></tr>";
-        }
-        if(val._level){ //primarily for autolevel stuffs
-          // str += "<table><tbody><tr><th>"+k.toTitleCase()+":</th><tr><table><tbody><tr>"
-          str += "<table><tbody><tr><th>Level:"+val._level+"</th><tr><table><tbody><tr>"
-          for(var j in val){
-            if(typeof val[j] == "object"){
-              for(var l in val[j]){ // print name and text, if text is array, loop over that
-                if(typeof val[j][l].text == "object"){
-                  for(var t in val[j][l].text){
-                    str += "<tr><td>"+val[j][l].text[t]+"</td></tr>";    
+      }else if(info[data] != undefined){ // has object or array of objects inside val
+        var str = "<table><tbody><tr><th class='trait'>"+data.toTitleCase()+":</th><td><table><tbody><tr>";
+        for(var k in info[data]){
+          var val = info[data][k];
+          if(val == "") continue;
+          if(val.name){
+            str += "<tr><td><b>" + val.name + "</b></td></tr>";
+          }
+          if(val.text){
+            str += "<tr><td>"+val.text+"</td></tr>";
+          }
+          if(val.attack){
+            str += "<tr><td><i>"+val.attack+"</i></td></tr>";
+          }
+          if(val._level){ //primarily for autolevel stuffs
+            // str += "<table><tbody><tr><th>"+k.toTitleCase()+":</th><tr><table><tbody><tr>"
+            str += "<table><tbody><tr><th>Level:"+val._level+"</th><tr><table><tbody><tr>"
+            for(var j in val){
+              if(typeof val[j] == "object"){
+                for(var l in val[j]){ // print name and text, if text is array, loop over that
+                  if(typeof val[j][l].text == "object"){
+                    for(var t in val[j][l].text){
+                      str += "<tr><td>"+val[j][l].text[t]+"</td></tr>";    
+                    }
+                  }else{
+                    str += "<tr><td><b>"+val[j][l].name+"</b></td></tr></tr><td>"+val[j][l].text+"</td></tr>";  
                   }
-                }else{
-                  str += "<tr><td><b>"+val[j][l].name+"</b></td></tr></tr><td>"+val[j][l].text+"</td></tr>";  
                 }
+              }else{
+                // str += "<tr><td>"+val[j].text+"</td></tr>"; //just printing level num? comment out
               }
-            }else{
-              // str += "<tr><td>"+val[j].text+"</td></tr>"; //just printing level num? comment out
-            }
-          } 
-          str += "</tr></tbody></table>"
+            } 
+            str += "</tr></tbody></table>"
+          }
         }
+        str += "</tbody></table></td></tr></tbody></table>";
+        $("#results").append(str);
       }
-      str += "</tbody></table></td></tr></tbody></table>";
-      $("#results").append(str);
     }
   }
+
   $("#results").show();
 
   //update url in address bar for linking
@@ -185,6 +214,14 @@ function setupTA(){
     templates: {
       header: '<h4 id="queryheader">Races</h4>'
     }
+  },
+  {
+    name: 'rules',
+    source: ruleSubstringMatcher(compendium.rules),
+    limit: 3,
+    templates: {
+      header: '<h4 id="queryheader">Rules</h4>'
+    }
   });
 }
 
@@ -210,12 +247,31 @@ function substringMatcher(strs){
   };  
 }
 
+function ruleSubstringMatcher(strs){
+  return function findMatches(q, cb) {
+    var matches, substringRegex;
+
+    matches = [];
+
+    substrRegex = new RegExp(q, 'i');
+
+    $.each(strs, function(i, str){
+      if(substrRegex.test(str.name)) {
+        matches.push(str.name);
+      }
+    });
+
+    cb(matches);
+  }
+}
+
 function getInfo(sug){
   var ret;
 
   for(var type in compendium){
     for(var data in compendium[type]){
       if(compendium[type][data].name == sug){
+        compendium[type][data].type = type;
         return compendium[type][data];
       }
     };
@@ -233,6 +289,12 @@ function populateBrowseMenus(){
     }
     $("#" + type + "-menu").append(str);;
   }
+}
+
+function populateChangelog(){
+  $("#changelog").popover({
+    content: '<ul id="changelog-list"><li><h6>July 12, 2016</h6>Added beginning of ruleset, look for abilities/skills. Added this. Search for Conditions to try out something cool. Hope you like!</li></ul>'
+  });
 }
 
 //------ UTIL methods
