9b140753bf1db45a2400127228aed0eff0e24686
Chris Jagusch <cjjagusc@mtu.edu>
2019-11-30 21:05:24 -0500

    revert pr

--- js/script.js
+++ js/script.js
@@ -54,7 +54,6 @@ function init(data){
   var q = decodeURI(location.search.substring(3)); // format: ?q=  - this is stripped out
   if(q != ""){
     q=q.toTitleCase();
-    q=q.replace(/\+/g, '%20');
     $('.typeahead').typeahead('val', q);
     createCard(getInfo(q));
   }
